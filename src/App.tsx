a3f2cb46ed29488adbb19ffb2b613ddd09c59def
Vivi <shoter998@gmail.com>
2022-10-30 11:36:33 +0100

    -Flags no longer repeat during session -After guessing all flags shows msg -Button to reset session after guessing all flags

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { country_data } from "./utils/country_list";
 import AnswerBox from "./components/AnswerBox";
+import { parseJsonText } from "typescript";
 
 function App() {
   function shuffle(array: number[]) {
@@ -28,21 +29,38 @@ function App() {
     threeLetterCode: string;
     twoLetterCode: string;
   }
-  const getRandomCountry = () => {
-    let randomCountry =
-      country_data[Math.floor(Math.random() * country_data.length)];
+  const [countriesUsed, setCountriesUsed] = useState(
+    sessionStorage.getItem("countriesUsed")! || ""
+  );
+  const getRandomCountry = (canRepeat = 0) => {
+    let randomCountry;
+    if (countriesUsed.length / 2 === country_data.length) canRepeat = 0;
+
+    if (canRepeat === 1) {
+      while (true) {
+        randomCountry =
+          country_data[Math.floor(Math.random() * country_data.length)];
+        if (!countriesUsed.includes(randomCountry.twoLetterCode)) break;
+      }
+    } else {
+      randomCountry =
+        country_data[Math.floor(Math.random() * country_data.length)];
+    }
     return randomCountry;
   };
   const [numbers, setNumbers] = useState([0, 1, 2, 3]);
-  const [countries, setCountries] = useState<Country[]>([getRandomCountry()]);
+  const [countries, setCountries] = useState<Country[]>([getRandomCountry(1)]);
   const [isAnswered, setIsAnswered] = useState(false);
   const [answeredCorrectly, setAnswerdCorrectly] = useState<number>(
     parseInt(sessionStorage.getItem("answeredCorrectly")!) || 0
   );
+
   const [answered, setAnswerd] = useState<number>(
     parseInt(sessionStorage.getItem("answered")!) || 0
   );
+  const countriesCount = country_data.length;
   useEffect(() => {
+    if (countriesUsed.length / 2 === country_data.length) return;
     let countryList = [...countries];
     for (let _ = 0; _ < 3; _++) {
       while (true) {
@@ -62,14 +80,18 @@ function App() {
     setNumbers(shuffle(numbers));
   }, []);
 
-  const changeScoreValues = (val: number) => {
+  const changeScoreValues = (val: number, code: string) => {
     setAnswerdCorrectly(answeredCorrectly + val);
     setAnswerd(answered + 1);
+    setCountriesUsed(`${code}${countriesUsed}`);
     sessionStorage.setItem(
       "answeredCorrectly",
       (answeredCorrectly + val).toString()
     );
     sessionStorage.setItem("answered", (answered + 1).toString());
+    if (val != 0) {
+      sessionStorage.setItem("countriesUsed", `${code}${countriesUsed}`);
+    }
   };
 
   return (
@@ -79,49 +101,67 @@ function App() {
         <div>Score</div>
         <div>{`${answeredCorrectly}/${answered}`}</div>
         <div>{Math.floor((answeredCorrectly / answered) * 100) || 0}%</div>
+        <div>Left: {countriesCount - answeredCorrectly}</div>
       </div>
       <div className="p-2"></div>
-      <div>
-        <header className="text-3xl">What country is it?</header>
-        <div className="p-4"></div>
-      </div>
-      <div className="border rounded text-center">
-        <img
-          src={
-            "https://countryflagsapi.com/png/" +
-            (countries[0].twoLetterCode || "").toLowerCase()
-          }
-          alt="Country Flag"
-        />
-      </div>
-      <div className="p-4"></div>
-      <div className="grid grid-cols-2 gap-6">
-        {countries.length == 4
-          ? numbers.map((num) => (
-              <AnswerBox
-                changeScore={changeScoreValues}
-                isAnswered={isAnswered}
-                goToNext={() => setIsAnswered(!isAnswered)}
-                key={countries[num].twoLetterCode}
-                country={countries[num]}
-                answer={num == 0 ? "this" : ""}
-              />
-            ))
-          : ""}
-      </div>
-      <div className="p-4"></div>
-      <div>
-        <button
-          className={`border rounded p-2 hover:bg-white hover:text-black cursor-pointer ${
-            !isAnswered && "hidden"
-          }`}
-          onClick={() => {
-            window.location.reload();
-          }}
-        >
-          Next
-        </button>
-      </div>
+      {countriesUsed.length / 2 === country_data.length && (
+        <>
+          <div>You have successfully guessed all flags</div>
+          <button
+            onClick={() => {
+              sessionStorage.clear();
+              window.location.reload();
+            }}
+          >
+            Reset
+          </button>
+        </>
+      )}
+      {countriesUsed.length / 2 !== country_data.length && (
+        <>
+          <div>
+            <header className="text-3xl">What country is it?</header>
+            <div className="p-4"></div>
+          </div>
+          <div className="border rounded text-center">
+            <img
+              src={
+                "https://countryflagsapi.com/png/" +
+                (countries[0].twoLetterCode || "").toLowerCase()
+              }
+              alt="Country Flag"
+            />
+          </div>
+          <div className="p-4"></div>
+          <div className="grid grid-cols-2 gap-6">
+            {countries.length == 4
+              ? numbers.map((num) => (
+                  <AnswerBox
+                    changeScore={changeScoreValues}
+                    isAnswered={isAnswered}
+                    goToNext={() => setIsAnswered(!isAnswered)}
+                    key={countries[num].twoLetterCode}
+                    country={countries[num]}
+                    answer={num == 0 ? "this" : ""}
+                  />
+                ))
+              : ""}
+          </div>
+          <div className="p-4"></div>
+          <div>
+            <button
+              className={`border rounded p-2 hover:bg-white hover:text-black cursor-pointer ${
+                !isAnswered && "hidden"
+              }`}
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/AnswerBox.tsx
+++ src/components/AnswerBox.tsx
@@ -1,50 +1,53 @@
-import React, { useEffect, useId, useRef } from 'react'
-interface Props{
-    isAnswered: boolean;
-    changeScore: (val: number) => void;
-    goToNext: () => void;
-    country: {
-        name: string[];
-        threeLetterCode: string;
-        twoLetterCode: string;
-
-    };
-    answer?: string;
+import React, { useEffect, useId, useRef } from "react";
+interface Props {
+  isAnswered: boolean;
+  changeScore: (val: number, code: string) => void;
+  goToNext: () => void;
+  country: {
+    name: string[];
+    threeLetterCode: string;
+    twoLetterCode: string;
+  };
+  answer?: string;
 }
 
-const AnswerBox = (props : Props) => {
+const AnswerBox = (props: Props) => {
+  const button = useId();
 
-  const button = useId()
-  
   useEffect(() => {
-    if(props.isAnswered){
-      let btn = document.getElementById(button)?.classList
+    if (props.isAnswered) {
+      let btn = document.getElementById(button)?.classList;
       btn?.remove("hover:text-black", "hover:bg-white", "cursor-pointer");
-      if(props.answer == "this"){
-        btn?.add("bg-green-500")
+      if (props.answer == "this") {
+        btn?.add("bg-green-500");
       }
     }
-  }, [props.isAnswered])
+  }, [props.isAnswered]);
 
-  const  checkAnswer = () => {
-    if (props.isAnswered){
-      return
+  const checkAnswer = () => {
+    if (props.isAnswered) {
+      return;
     }
-    let btn = document.getElementById(button)?.classList
+    let btn = document.getElementById(button)?.classList;
     btn?.remove("hover:text-black", "hover:bg-white", "cursor-pointer");
-    props.goToNext()
-    if(props.answer == "this"){
-      props.changeScore(1)
-      btn?.add("bg-green-500")
-    }else{
-      props.changeScore(0)
-      btn?.add("bg-red-500")
+    props.goToNext();
+    if (props.answer == "this") {
+      props.changeScore(1, props.country.twoLetterCode);
+      btn?.add("bg-green-500");
+    } else {
+      props.changeScore(0, props.country.twoLetterCode);
+      btn?.add("bg-red-500");
     }
+  };
+  return (
+    <div
+      id={button}
+      onClick={checkAnswer}
+      className={`border text-center rounded p-6 md:p-12 hover:bg-white cursor-pointer hover:text-black`}
+    >
+      {props.country.name[0] || ""}
+    </div>
+  );
+};
 
-  }
-  return ( 
-    <div id={button} onClick={checkAnswer} className={`border text-center rounded p-6 md:p-12 hover:bg-white cursor-pointer hover:text-black`}>{props.country.name[0] || ""}</div>
-  )
-}
-
-export default AnswerBox
+export default AnswerBox;
